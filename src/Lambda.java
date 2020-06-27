4cb032c633a67bda8b220e745faf18397b2c6756
SibilJoeKurian <Sibil.kurian@gmail.com>
2020-06-26 23:05:15 -0400

    lambda using interface

---
+++ src/Lambda.java
@@ -0,0 +1,12 @@
+/**
+ * Lambda function using interface
+ * This class contains a small program of lambda function
+ */
+public class Lambda {
+    public static void main(String[] args){
+     add addFucntion=(int a)-> a+a;
+    }
+}
+interface add{
+ int addFucntion(int a);
+}
